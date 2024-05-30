a4f057634262210b31a73ff44c0f9f5248a12c79
fagomes <falmeidagomes13@gmail.com>
2024-05-30 13:37:48 -0300

    changes

--- src/main/java/com/felipeagomes/Main.java
+++ src/main/java/com/felipeagomes/Main.java
@@ -1,21 +1,68 @@
 package com.felipeagomes;
 
+import com.felipeagomes.entities.ProductsReceipts;
 import com.felipeagomes.products.Product;
 import com.felipeagomes.products.ProductAggregator;
 import com.felipeagomes.receipt.ReceiptReader;
 import com.felipeagomes.service.ExcelReceiptBuilder;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.apache.poi.hpsf.HPSFRuntimeException;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        ReceiptReader receiptReader = getReceiptReader();
+        List<ProductsReceipts> productsReceipts = receiptReaderToProductsReceipts(receiptReader);
+
+        EntityTransaction tr = null;
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("ReceipReader");
+             EntityManager em = emf.createEntityManager()) {
+
+            tr = em.getTransaction();
+            tr.begin();
+
+            for (ProductsReceipts productReceipt : productsReceipts) {
+                em.persist(productReceipt);
+            }
+
+            tr.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            if (tr != null && tr.isActive()) {
+                tr.rollback();
+            }
+        }
+
+    }
+
+    private static List<ProductsReceipts> receiptReaderToProductsReceipts(ReceiptReader receiptReader) {
+        List<ProductsReceipts> productsReceipts = new ArrayList<>();
+        for (Product product : receiptReader.getProducts()) {
+            BigDecimal valor = BigDecimal.valueOf(product.getValor());
+            ProductsReceipts productReceipt = new ProductsReceipts(product.getCodigo(), product.getNome(), product.getQuantidade(), product.getUnidade(), valor);
+            productReceipt.setSupermarket(receiptReader.getCompanyName());
+            productReceipt.setPurchaseDate(receiptReader.getDate());
+
+            productsReceipts.add(productReceipt);
+        }
+
+        return productsReceipts;
+    }
+
+    private static void createExcelReceipt() {
         final String XLSX_EXTENSION = ".xls";
 
-        ReceiptReader receiptReader = new ReceiptReader(new File("C:\\Users\\falme\\Downloads\\receipt.pdf"));
-
-        receiptReader.setProducts(aggregateProducts(receiptReader));
+        ReceiptReader receiptReader = getReceiptReader();
 
         Path resultPath = Path.of("C:\\Users\\falme\\Downloads\\" + receiptReader.getTitle() + XLSX_EXTENSION);
 
@@ -27,7 +74,13 @@ public class Main {
                 .setTitle(receiptReader.getTitle());
 
         excelBuilder.create();
+    }
 
+    private static ReceiptReader getReceiptReader() {
+        ReceiptReader receiptReader = new ReceiptReader(new File("C:\\Users\\falme\\Downloads\\Consulta Pública de NFCe (2).pdf"));
+        receiptReader.setProducts(aggregateProducts(receiptReader));
+
+        return receiptReader;
     }
 
     private static List<Product> aggregateProducts(ReceiptReader receiptReader) {
---
+++ src/main/java/com/felipeagomes/entities/ProductsReceipts.java
@@ -0,0 +1,110 @@
+package com.felipeagomes.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "products_recepts")
+public class ProductsReceipts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "product_name", length = 512, nullable = false)
+    private String productName;
+
+    @Column(name = "quantity", nullable = false)
+    private Double quantity;
+
+    @Column(name = "value", nullable = false)
+    private BigDecimal value;
+
+    @Column(name = "unit", length = 2, nullable = false)
+    private String unit;
+
+    @Column(name = "purchase_date", nullable = false)
+    private Date purchaseDate;
+
+    @Column(name = "supermarket", nullable = false)
+    private String supermarket;
+
+    public ProductsReceipts() {}
+
+    public ProductsReceipts(String code, String productName, Double quantity, String unit, BigDecimal value) {
+        this.code = code;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.value = value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public String getSupermarket() {
+        return supermarket;
+    }
+
+    public void setSupermarket(String supermarket) {
+        this.supermarket = supermarket;
+    }
+}
